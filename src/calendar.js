10142e79b8492ff4ae1e50294720ff23318b24c9
dmitriy144 <dmitriy.kilchitsky@gmail.com>
2021-02-02 21:58:52 +0200

    Adding SCSS

--- src/calendar.js
+++ src/calendar.js
@@ -23,13 +23,11 @@ function renderCalendar(filter) {
     const tableCell = row.querySelector(`td:nth-child(${event.day})`);
 
     const eventBox = document.createElement('div');
-    const eventName = document.createElement('p');
-    const deleteBtn = document.createElement('div');
-    eventName.textContent = `${event.name}`;
-    deleteBtn.textContent = 'X';
-    eventBox.appendChild(eventName);
-    eventBox.appendChild(deleteBtn);
-    eventBox.setAttribute('data-local-storage-key', `_event${event.day}${event.time}`);
+    eventBox.innerHTML = `<p>${event.name}</p><div class="delete-btn">X</div>`;
+    eventBox.setAttribute(
+      'data-local-storage-key',
+      `_event${event.day}${event.time}`,
+    );
     eventBox.classList.add('event_box');
 
     tableCell.classList.add('event_cell');
@@ -37,11 +35,12 @@ function renderCalendar(filter) {
   });
 }
 
-document.querySelector('#table').addEventListener('click', (event) => {
-  const currentCell = event.target.parentNode.parentNode;
-  const calendarEventBox = event.target.parentNode;
-  if (calendarEventBox.hasAttribute('data-local-storage-key')) {
+document.querySelector('#meeting_room').addEventListener('click', (event) => {
+  if (event.target.classList.contains('delete-btn')) {
+    const currentCell = event.target.parentNode.parentNode;
+    const calendarEventBox = event.target.parentNode;
     const eventName = calendarEventBox.querySelector('p').textContent;
+
     if (confirm(`Are you sure you want to delete "${eventName}" event?`)) {
       localStorage.removeItem(calendarEventBox.dataset.localStorageKey);
       currentCell.removeChild(calendarEventBox);
@@ -50,16 +49,18 @@ document.querySelector('#table').addEventListener('click', (event) => {
   }
 });
 
-document.querySelector('#team > select').addEventListener('change', (event) => {
-  const eventCells = Array.from(document.querySelectorAll('.event_cell'));
+document
+  .querySelector('#members > select')
+  .addEventListener('change', (event) => {
+    const eventCells = Array.from(document.querySelectorAll('.event_cell'));
 
-  eventCells.forEach((eventCell) => {
-    const calendarEventBox = eventCell.childNodes[0];
-    eventCell.removeChild(calendarEventBox);
-    eventCell.classList.remove('event_cell');
+    eventCells.forEach((eventCell) => {
+      const calendarEventBox = eventCell.childNodes[0];
+      eventCell.removeChild(calendarEventBox);
+      eventCell.classList.remove('event_cell');
+    });
+
+    renderCalendar(event.target.value);
   });
 
-  renderCalendar(event.target.value);
-});
-
-renderCalendar(document.querySelector('#team > select').value);
+renderCalendar(document.querySelector('#members > select').value);
